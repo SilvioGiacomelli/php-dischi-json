280dab7b631dc23808e6f562687905fad994dbc8
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-04-17 15:53:44 +0200

    Array stampato in pagina

--- index.php
+++ index.php
@@ -18,9 +18,10 @@
     <div v-for="(album, index) in list" :key=index class="card" style="width: 18rem;">
       <img class="card-img-top" :src="album.poster" alt="Card image cap">
       <div class="card-body">
-        <h5 class="card-title">...</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">{{album.title}}</h5>
+        <p class="card-text">{{album.author}}</p>
+        <p class="card-text">{{album.genre}}</p>
+        <p class="card-text">{{album.year}}</p>
       </div>
     </div>
   </div>
